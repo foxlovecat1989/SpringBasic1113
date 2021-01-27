bc9e0f8b8c542fa6edb7eeb59ac8ac6fe4b02f08
foxlo <foxlo@DESKTOP-4R8BSQ7>
2021-01-27 15:47:32 +0800

    AOP

--- src/main/java/com/mycompany/springbasic1113/aop/LogAspect.java
+++ src/main/java/com/mycompany/springbasic1113/aop/LogAspect.java
@@ -1,7 +1,12 @@
 package com.mycompany.springbasic1113.aop;
 
+import java.util.Arrays;
+import org.aopalliance.intercept.Joinpoint;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -10,11 +15,30 @@ import org.springframework.stereotype.Component;
 @Order(1) // 數字越小越先被執行
 public class LogAspect {
     
+    @Pointcut(value = "execution(* com.mycompany.springbasic1113.aop.*.*(..))")
+    public void cut() {
+    }
     // 前置通知(Advice)
     // 切入點表達式
     // @Before(value = "execution(public int com.mycompany.springbasic1113.aop.CalcImpl.add(int, int))")
-    @Before(value = "execution(public int com.mycompany.springbasic1113.aop.CalcImpl.*(int, int))") // 切入點表達式
-    public void before(){
-        System.out.println("前置通知:");
+    // @Before(value = "execution(public int com.mycompany.springbasic1113.aop.CalcImpl.*(int, int))") // 切入點表達式
+    // @Before(value = "execution(public int com.mycompany.springbasic1113.aop.*.*(int, int))") // 切入點表達式
+    // @Before(value = "execution(* com.mycompany.springbasic1113.aop.*.*(..))") // 切入點表達式
+    // @Before(value = "execution(* *.*(..))") // 切入點表達式
+    @Before(value = "cut()") // 切入點參照
+    // JoinPoint 指的就是作用的方法
+    public void before(JoinPoint joinPoint){
+         // 獲取方法名稱
+        String methodName = joinPoint.getSignature().getName();
+        // 獲取方法參數
+        Object[] args = joinPoint.getArgs();
+        System.out.printf("前置通知 -> 方法名稱: %s, 方法參數: %s\n", methodName, Arrays.toString(args));
+    }
+    
+    // 後置通知(Advice)
+    // 無論是否有例外發生都會被執行, 因後置通知作用在 finally 中...
+    @After(value = "cut()") // 切入點參照
+    public void after() {
+        System.out.println("後置通知:");
     }
 }
--- src/test/java/aop/Test1.java
+++ src/test/java/aop/Test1.java
@@ -10,10 +10,10 @@ public class Test1 {
         ClassPathXmlApplicationContext ctx = 
                 new ClassPathXmlApplicationContext("aop-config.xml");
         Calc calc = ctx.getBean("calcImpl", Calc.class);
-        System.out.println(calc.add(2, 3));
-        System.out.println(calc.sub(2, 3));
-        System.out.println(calc.mul(2, 3));
-        System.out.println(calc.div(3, 3));
+        // System.out.println(calc.add(2, 3));
+        // System.out.println(calc.sub(2, 3));
+        // System.out.println(calc.mul(2, 3));
+        System.out.println(calc.div(4, 2));
         ctx.close();
     }
 }
